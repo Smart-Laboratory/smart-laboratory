beb7a47e6a046ea1afe43c7520ac400e17cabe03
qwater <yuzhihua10000@126.com>
2016-10-31 15:45:01 +0800

    体检接收bug
    体检接收bug

--- src/main/java/com/smart/webapp/controller/manage/SampleInputAjaxController.java
+++ src/main/java/com/smart/webapp/controller/manage/SampleInputAjaxController.java
@@ -958,7 +958,7 @@ public class SampleInputAjaxController {
             for (LabOrder labOrder1 : labOrders) {
                 String itemCode = ConvertUtil.null2String(labOrder1.getYlxh());
                 String itemName = ConvertUtil.null2String(labOrder1.getExamitem());
-                Ylxh ylxh = YlxhUtil.getInstance().getYlxh(sample.getYlxh().split("[+]")[0]);
+                Ylxh ylxh = YlxhUtil.getInstance().getYlxh(labOrder1.getYlxh());
                 Float amount = ConvertUtil.getFloatValue(ylxh.getPrice());
                 if (!itemCode.isEmpty() && !ConvertUtil.null2String(sample.getYlxh()).isEmpty()) {
                     itemCode = "+" + itemCode;
