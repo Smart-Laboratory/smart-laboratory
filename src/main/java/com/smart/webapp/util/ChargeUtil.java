52122d0bcdfc2ad075c73106245de14aeeda0577
zcwsjx <zhouchenwei520@163.com>
2016-11-01 11:13:27 +0800

    采血适应项目

--- src/main/java/com/smart/webapp/util/ChargeUtil.java
+++ src/main/java/com/smart/webapp/util/ChargeUtil.java
@@ -100,6 +100,7 @@ public class ChargeUtil {
                 //param.put("price", "");
                 //param.put("testPurposesCode", SamplingSitesUtil.getValue(ConvertUtil.null2String(labOrder.getToponymy())));   //获取费用项目ID
                 param.put("feeItemCode","10054");
+                param.put("fitItemId","0");
                 //param.put("feeItemName","静脉采血");
                 //param.put("feeItemName", "");
                 param.put("billingDoctorNo", ConvertUtil.null2String(labOrder.getRequester()));
@@ -123,6 +124,7 @@ public class ChargeUtil {
                 //param.put("price", "");
                // param1.put("testPurposesCode", SamplingSitesUtil.getValue(labOrder.getToponymy() + "采血针"));   //获取费用项目ID
                 param1.put("feeItemCode","16040");
+                param1.put("fitItemId","0");
                 //param1.put("feeItemName","静脉采血器(针头)(BD 21G)");
                 //param.put("feeItemName", "");
                 param1.put("billingDoctorNo", ConvertUtil.null2String(labOrder.getRequester()));
@@ -174,15 +176,18 @@ public class ChargeUtil {
             param.put("patientName", labOrder.getPatientname());
             param.put("dateTime", ConvertUtil.getFormatDateGMT(labOrder.getExecutetime(),"yyyy-MM-dd'T'HH:mm:ss'Z'" ));//yyyy-mm-dd hh24:mi:ss
             param.put("quantity", "1");
+            param.put("fitItemId","0");
             //param.put("price", "");
             //param.put("testPurposesCode", TestTubeUtil.getInstance(testTubeManager).getValue(labOrder.getContainer()));   //获取费用项目ID
             if(labOrder.getContainer().equals("红色管")) {
                 param.put("feeItemCode", "16041");
                 //param.put("feeItemName", "");
                 param.put("feeItemName","采血器(普通真空试管)");
+                param.put("fitItemId","10054");
             } else {
                 param.put("feeItemCode", "16042");
                 param.put("feeItemName","采血器(特殊真空试管)");
+                param.put("fitItemId","10054");
             }
 
             param.put("billingDoctorNo", labOrder.getRequester());
